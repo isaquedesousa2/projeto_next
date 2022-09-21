e6dfb5a46889617f723d693bdec888ea99970494
isaquedesousa2 <isaqueleal35@gmail.com>
2022-09-21 17:12:17 -0300

    Melhorando a pagina FAQ

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -1,5 +1,6 @@
-import Link from "../src/components/Link/Link";
-import Head from 'next/head';
+import FAQScreen from '../src/screens/FAQScreen';
+
+export default FAQScreen; 
 
 export async function getStaticProps(){
     const url = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
@@ -15,30 +16,4 @@ export async function getStaticProps(){
             faq
         }
     }
-}
-
-export default function FAQPage({faq}){
-
-    return(
-        <div>
-            <Head>
-                <title>Home - Alura Cases</title>
-            </Head>
-            <h1>Alura Cases - FAQ</h1>
-            <Link href={'/'}>
-                Ir para Home
-            </Link>
-            {
-            faq.map(({ answer, question }) => (
-                <li key={question}>
-                    <article>
-                        <h2>{question}</h2>
-                        <p>{answer}</p>
-                    </article>
-                </li>
-            ))
-        }
-        </div>
-        
-    )
 }
